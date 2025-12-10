41bcd5fb34eaf1a66b62ed426e52872b49ae054e
DanMorgan6 <daniel@beacheschiro.co.uk>
2025-12-10 00:14:35 +0000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,19 @@
-self.addEventListener('fetch',e=>{});
+const CACHE_NAME = 'knee-oa-cache-v2';
+const URLS_TO_CACHE = [
+  'index.html',
+  'app.js',
+  'manifest.json',
+  'logo.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
